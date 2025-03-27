f16d64fa403c0a9eb381dbdcf5c1238722df1179
NebularEclipse <143008010+NebularEclipse@users.noreply.github.com>
2025-03-27 04:02:06 +0000

    Trying something again

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
+import math
+
 import streamlit as st
 
-st.title("🎈 My new app - GIAN CARLO BATA")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def is_prime(n):
+    if n == 2 or n == 3:
+        return True
+    elif n <= 1 or n % 2 == 0 or n % 3 == 0:
+        return False
+    
+    for i in range(5, int(math.sqrt(n)) + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        
+    return True
+
+number = st.number_input(label="Select a prime number", min_value=2)
+if is_prime(number):
+    st.write("Prime")
+else:
+    st.write("Not Prime")
